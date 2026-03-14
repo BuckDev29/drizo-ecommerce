f3cc3b6565b5529dc3da4c5d7361b497f8fead90
BuckDev29 <mirandacamilo2005@gmail.com>
2026-03-14 14:12:21 -0500

    DB Public url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,7 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = mysql.createPool(connectionString);
 
 module.exports = pool.promise();
